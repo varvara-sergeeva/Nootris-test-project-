a70b326362c98e610a9482fd9fb201b9da09e471
varvara-sergeeva <raunuperetemheru@gmail.com>
2022-07-27 22:26:09 +0300

    Исправление бага бургерного меню

--- source/js/menu.js
+++ source/js/menu.js
@@ -3,6 +3,14 @@ const burgerToggle = document.querySelectorAll('.navigation__burger');
 const burgerMenu = document.querySelector('.navigation__menu');
 const burgerOptions = document.querySelector('.navigation__options');
 const burgerBackStatic = document.querySelector('.page');
+const onMediaTablet = window.matchMedia('(min-width: 992px)');
+const closeBurger = n => {
+  for(let toggle of burgerToggle) {
+    toggle.classList.remove('navigation__burger--active');
+  }
+}
+
+onMediaTablet.addEventListener('change', closeBurgerOnTablet);
 
 burgerToggle.forEach(function (item) {
   item.addEventListener('click', () => {
@@ -12,3 +20,12 @@ burgerToggle.forEach(function (item) {
     burgerBackStatic.classList.toggle('page--burger');
   })
 });
+
+function closeBurgerOnTablet(e) {
+  if (e.matches) {
+    closeBurger();
+    burgerMenu.classList.remove('menu--active');
+    burgerOptions.classList.remove('navigation__options--burger');
+    burgerBackStatic.classList.remove('page--burger');
+  }
+};
