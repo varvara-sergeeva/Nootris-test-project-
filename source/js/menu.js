4d0f93109178aae0c27f4e16667781203400b0d3
varvara-sergeeva <raunuperetemheru@gmail.com>
2022-07-27 23:45:10 +0300

    оптимизация кода бургера

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,31 +1,21 @@
 "use strict";
-const burgerToggle = document.querySelectorAll('.navigation__burger');
+const burgerToggle = document.querySelector('.navigation__burger');
 const burgerMenu = document.querySelector('.navigation__menu');
-const burgerOptions = document.querySelector('.navigation__options');
 const burgerBackStatic = document.querySelector('.page');
 const onMediaTablet = window.matchMedia('(min-width: 992px)');
-const closeBurger = n => {
-  for(let toggle of burgerToggle) {
-    toggle.classList.remove('navigation__burger--active');
-  }
-}
 
 onMediaTablet.addEventListener('change', closeBurgerOnTablet);
 
-burgerToggle.forEach(function (item) {
-  item.addEventListener('click', () => {
-    item.classList.toggle('navigation__burger--active');
-    burgerMenu.classList.toggle('menu--active');
-    burgerOptions.classList.toggle('navigation__options--burger');
-    burgerBackStatic.classList.toggle('page--burger');
-  })
+burgerToggle.addEventListener('click', () => {
+  burgerToggle.classList.toggle('navigation__burger--active');
+  burgerMenu.classList.toggle('menu--active');
+  burgerBackStatic.classList.toggle('page--burger');
 });
 
 function closeBurgerOnTablet(e) {
   if (e.matches) {
-    closeBurger();
+    burgerToggle.classList.remove('navigation__burger--active');
     burgerMenu.classList.remove('menu--active');
-    burgerOptions.classList.remove('navigation__options--burger');
     burgerBackStatic.classList.remove('page--burger');
   }
 };
